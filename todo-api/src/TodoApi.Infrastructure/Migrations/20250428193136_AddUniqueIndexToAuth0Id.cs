ddc693ff3bc7939328d7e929c2770acab757f33b
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-04-29 09:22:13 +0530

    Make auth0id unique

---
+++ todo-api/src/TodoApi.Infrastructure/Migrations/20250428193136_AddUniqueIndexToAuth0Id.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToAuth0Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Auth0Id",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Auth0Id",
+                table: "Users",
+                column: "Auth0Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Auth0Id",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Auth0Id",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
